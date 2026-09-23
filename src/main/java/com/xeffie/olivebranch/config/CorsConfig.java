be40a36d8b85bb6c4c3f5a8936627e699b2eae24
Josefin Vingeskog <151967701+xeffie@users.noreply.github.com>
2026-09-23 09:45:22 +0200

    feat: configure CORS for frontend clients (#9)

---
+++ src/main/java/com/xeffie/olivebranch/config/CorsConfig.java
@@ -0,0 +1,24 @@
+package com.xeffie.olivebranch.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api")
+                .allowedOrigins(
+                        "http://127.0.0.1:5500",
+                        "http://localhost:5500",
+                        "https://olive-branch-web-dev.onrender.com",
+                        "https://olive-branch-web-prod.onrender.com"
+                )
+                .allowedMethods("GET");
+
+
+    }
+}
